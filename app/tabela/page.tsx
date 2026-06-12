6442b9c422c3a8f3a5f91c12644bae0fd2a54009
Marcin <marcinsajniak@gmail.com>
2026-06-12 10:36:12 +0200

    feat(app): widok tabeli ogolnej

---
+++ app/tabela/page.tsx
@@ -0,0 +1,12 @@
+import { RetroTable } from '../../components/RetroTable';
+import { ScreenFrame } from '../../components/ScreenFrame';
+import { loadResults } from '../lib/results';
+
+export default function TabelaPage() {
+  const { general } = loadResults();
+  return (
+    <ScreenFrame title="TABELA OGÓLNA">
+      <RetroTable rows={general} showGroup />
+    </ScreenFrame>
+  );
+}
